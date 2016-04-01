832d08237f065ef69a3beb18c60858675009d4f7
kucuny <kucuny@gmail.com>
2016-04-01 19:27:39 +0900

    Init refactoring code

--- conn.go
+++ conn.go
@@ -2,48 +2,115 @@ package redis
 
 import (
 	"fmt"
+	rg "github.com/garyburd/redigo/redis"
 	"github.com/kucuny/redigocon"
+	"time"
 )
 
-type Connection interface {
-	RedisCommands
-	ConnectionCommands
+var DefaultRedisPoolConfig = RedisPoolConfig{
+	MaxIdle:     60,
+	MaxActive:   100,
+	IdleTimeout: 30,
 }
 
-func CreateConnection(serverAddr, auth, db string) (Connection, error) {
-	c, e := redigocon.Connect(serverAddr, auth, db)
+type RedisConnConfig struct {
+	host string
+	port int
+	auth string
+	db   int
+}
 
-	if e != nil {
-		fmt.Println(e)
-
-		return nil, e
+func NewRedisConnConfig(host string, port int, auth string, db int) RedisConnConfig {
+	return RedisConnConfig{
+		host: host,
+		port: port,
+		auth: auth,
+		db:   db,
 	}
-
-	con := &connection{c: c}
-
-	return con, nil
 }
 
-func CreateConnectionUri(uri string) (Connection, error) {
-	c, e := redigocon.ConnectUrl(uri)
-
-	if e != nil {
-		fmt.Println(e)
-
-		return nil, e
-	}
-
-	con := &connection{c: c}
-
-	return con, nil
+type RedisPoolConfig struct {
+	RedisConnConfig
+	MaxIdle     int
+	MaxActive   int
+	IdleTimeout time.Duration
 }
 
-func (con *connection) Close() bool {
-	err := con.c.Close()
+func NewRedisPoolConfig(host string, port int, auth string, db int) RedisPoolConfig {
+
+}
+
+type RedigoWrap struct {
+	con rg.Conn
+}
+
+type RedigoWrapPool struct {
+	pool *rg.Pool
+}
+
+type RedigoWrapPubSub struct {
+	pubsubCon *rg.PubSubConn
+}
+
+func NewRedigoWrap(config RedisConnConfig) (*RedigoWrap, error) {
+	var err error
+
+	address := fmt.Sprintf("redis://%s:%d/%d", config.host, config.port, config.db)
+
+	fmt.Println(address)
+	con, err := rg.DialURL(address)
 
 	if err != nil {
-		return false
+		return nil, err
 	}
 
-	return true
+	if config.auth != "" {
+		_, err = con.Do("AUTH", config.auth)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = con.Do("SELECT", config.db)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &RedigoWrap{
+		con: con,
+	}, nil
+}
+
+func NewRedigoWrapPool(uri string, poolConfig RedisPoolConfig) (PoolConnection, error) {
+	dialer := func() (rg.Conn, error) {
+		return redigocon.ConnectUrl(uri)
+	}
+
+	tester := func(c rg.Conn, t time.Time) error {
+		_, err := c.Do("PING")
+		return err
+	}
+
+	config := getConnectionPoolConfig(&poolConfig)
+
+	pool := &rg.Pool{
+		MaxIdle:      config.MaxIdle,
+		IdleTimeout:  config.IdleTimeout,
+		Dial:         dialer,
+		TestOnBorrow: tester,
+	}
+
+	con := &connection{p: pool}
+
+	return con, nil
+}
+
+func getConnectionPoolConfig(config *RedisPoolConfig) RedisPoolConfig {
+	if config.IdleTimeout == 0 || config.MaxIdle == 0 || config.MaxActive == 0 {
+		return DefaultRedisPoolConfig
+	} else {
+		return *config
+	}
 }
---
+++ conn_test.go
@@ -0,0 +1,29 @@
+package redis
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type RedigoWrapConnectionTest struct {
+	suite.Suite
+}
+
+func (suite *RedigoWrapConnectionTest) SetupSuite() {
+}
+
+func (suite *RedigoWrapConnectionTest) TestRedisConnection() {
+	connConfig := NewRedisConnConfig("localhost", 6379, "", 0)
+	redis, err := NewRedigoWrap(connConfig)
+
+	suite.Nil(err)
+	suite.NotNil(redis)
+}
+
+func (suite *RedigoWrapConnectionTest) TestRedisPoolConnection() {
+
+}
+
+func TestRedigoWrapConnectionTestSuite(t *testing.T) {
+	suite.Run(t, new(RedigoWrapConnectionTest))
+}
